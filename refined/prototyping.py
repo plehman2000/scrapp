5bded67ef698368d2eb77a92be939df34ee5459b
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2024-10-26 17:12:20 -0400

    reorg

--- refined/prototyping.py
+++ refined/prototyping.py
@@ -7,14 +7,13 @@ app = marimo.App(width="medium")
 @app.cell
 def __():
     from prover import Prover
-
     prover = Prover()
     return Prover, prover
 
 
 @app.cell
 def __(prover):
-    claim = "Abortion is moral"
+    claim = "The minecraft youtuber Dream is a pedophile"
     out = None
     for x in prover.run(proposition_claim=claim, use_small_model=False):
         out = x
@@ -24,6 +23,68 @@ def __(prover):
     print(arg1_w_claims, arg2_w_claims)
     print(f"Winning Claim: {out['victor']}")
     return arg1_w_claims, arg2_w_claims, claim, out, x
+
+
+@app.cell
+def __(out):
+    out # should \grou[ all chunks together, just in case there is one informative source?]
+    ####
+    #"proposition_query":
+    #"Why do you believe that most dogs are friendly?"
+    #FUCKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+    return
+
+
+app._unparsable_cell(
+    r"""
+    {
+    \"proposition_claim\":
+    \"The minecraft youtuber Dream is a pedophile\"
+    \"status\":
+    \"Complete\"
+    \"progress\":
+    100
+    \"opposition_claim\":
+    \"The minecraft youtuber Dream is not a pedophile.\"
+    \"proposition_query\":
+    \"Why do you believe that most dogs are friendly?\"
+    \"opposition_query\":
+    \"Why isn't the Minecraft youtuber Dream considered a pedophile?\"
+    \"prop_final_args\":
+    []
+    \"prop_chunk\":
+    {
+    0:
+    []
+    1:
+    []2:
+    []
+    }
+    \"opp_final_args\":
+    []
+    \"opp_chunks\":
+    {
+    0:
+    []
+    1:
+    []2:
+    []
+    }
+    \"arg1_w_claims\":
+    \"Claim:The minecraft youtuber Dream is a pedophile\n\"
+    \"arg2_w_claims\":
+    \"Claim: The minecraft youtuber Dream is not a pedophile.\n\"
+    \"victor\":
+    \"The minecraft youtuber Dream is not a pedophile.\"
+    \"final_judge\":
+    {
+    \"argument\":
+    \"2\"
+    }
+    }
+    """,
+    name="__"
+)
 
 
 if __name__ == "__main__":
